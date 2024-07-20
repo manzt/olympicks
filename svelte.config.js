1b6f1ec117284d0d1f0d04d6031d4bac1317bfe3
Trevor Manz <trevor.j.manz@gmail.com>
2024-07-20 14:18:28 -0400

    chore: Remove BASE_PATH

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,6 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
 	kit: {
 		adapter: adapter({ fallback: "404.html" }),
-		paths: {
-			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
-		},
 	},
 	compilerOptions: { runes: true },
 };
